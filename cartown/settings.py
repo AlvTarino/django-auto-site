a3e789625f74a05eb8d834ce148c80f728e22ff9
AlvTarino <alvtarino@outlook.com>
2021-06-30 20:26:37 +0300

    Added templates, Set up Postgres db

--- cartown/settings.py
+++ cartown/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pages.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +77,11 @@ WSGI_APPLICATION = 'cartown.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'postgres',
+        'HOST': 'localhost',
+        'PASSWORD': '2909ORISHABA',  # os.environ.get('PASSWORD'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangoCars',
     }
 }
 
@@ -118,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # BASE_DIR / 'static'
+STATICFILES_DIR = [os.path.join(BASE_DIR, 'cartown/static'), ]
